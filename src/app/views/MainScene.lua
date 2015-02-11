154b2e9af1bc800e080ea78fce576a6fdbd93caa
Naoyuki Totani <nyk.totani@gmail.com>
2015-02-11 21:48:32 +0900

    show touch point

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -15,6 +15,15 @@ function MainScene:onCreate()
     cc.Menu:create(playButton)
         :move(display.cx, display.cy - 200)
         :addTo(self)
+
+    -- add touch layer
+    display.newLayer()
+        :onTouch(function(e)
+            display.newSprite("Star.png", e.x, e.y)
+            :addTo(self)
+            :runAction(cc.Sequence:create(cc.FadeOut:create(0.5), cc.RemoveSelf:create()))
+        end)
+        :addTo(self)
 end
 
 return MainScene
